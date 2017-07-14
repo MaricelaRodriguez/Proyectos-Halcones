1adeca67aef683f7dd2aba7f3357cb01e40f4e06
MaricelaRodriguez <mari.r.n.xd@gmail.com>
2017-07-14 17:08:11 -0500

    client_properties

--- halcones-eureka-cliente/src/main/java/mx/com/halcones/halconeseurekacliente/HalconesEurekaClienteApplication.java
+++ halcones-eureka-cliente/src/main/java/mx/com/halcones/halconeseurekacliente/HalconesEurekaClienteApplication.java
@@ -2,7 +2,9 @@ package mx.com.halcones.halconeseurekacliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HalconesEurekaClienteApplication {
 
