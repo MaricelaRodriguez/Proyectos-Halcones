e66bab91f5db2ce1efecf126bf3bc0c6deabb8ed
MaricelaRodriguez <mari.r.n.xd@gmail.com>
2017-07-14 16:39:47 -0500

    Cliente

---
+++ halcones-eureka-cliente/src/main/java/mx/com/halcones/halconeseurekacliente/HalconesEurekaClienteApplication.java
@@ -0,0 +1,12 @@
+package mx.com.halcones.halconeseurekacliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalconesEurekaClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalconesEurekaClienteApplication.class, args);
+	}
+}
